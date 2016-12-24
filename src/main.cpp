773de505ed0720e0ed5a4235a7d699cc3d955c6b
Yonatan Oren <yonatano@gmail.com>
2016-12-24 18:19:30 -0500

    some util functions, .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <set>
+#include <vector>
 #include <armadillo>
 
 // #include "decisiontree.h"
@@ -9,6 +11,14 @@
 using namespace std;
 using namespace arma;
 
+// forward-declaration headers
+map<string, vector<string> > loadCSV(string fileName);
+void printCSV(map<string, vector<string> >);
+void labelEncodeData(map<string, vector<string> > dat);
+vector<string> uniqueElems(vector<string> v);
+int indexInVector(vector<int> v, int s);
+
+// loads CSV data into a map from column_names -> [data] given a fileName
 map<string, vector<string> > loadCSV(string fileName) {
     map<string, vector<string> > data;
     ifstream file(fileName);
@@ -33,6 +43,7 @@ map<string, vector<string> > loadCSV(string fileName) {
     return data;
 } 
 
+// prints a structure generated with loadCSV
 void printCSV(map<string, vector<string> > dat) {
     for (auto m = dat.begin(); m != dat.end(); m++) {
         string col = m->first;
@@ -45,7 +56,40 @@ void printCSV(map<string, vector<string> > dat) {
     }
 }
 
+// replaces all categorical entries with a corresponding list index 
+void labelEncodeData(map<string, vector<string> > dat) {
+    for (auto m = dat.begin(); m != dat.end(); m++)  {
+        string key = m->first;
+        vector<string> ftrs = uniqueElems(m->second);
 
+    }
+}
+
+// generates a matrix from a data map. y is the key corresponding to the desired
+// predictions.
+// returns X, a matrix containing the features. y, a column vector containing
+// the desired predictions.
+void dataToMatrix(map<string, vector<string> > dat, string y) {
+    // xsz: # of features, length(dat.keys())
+    // ysz: # of outcomes, length(uniqueElems(dat[y]))
+    // n: # of data samples, (dat[0].size())
+    // X: mat(n, xzs)
+    // Y: mat(n, 1)
+    // numelems = 
+    // mat data = ()
+}
+
+/* Util Functions */
+vector<string> uniqueElems(vector<string> v) {
+    set<string> s(v.begin(), v.end());
+    vector<string> vec(s.begin(), s.end());
+    return vec;
+}
+
+int indexInVector(vector<int> v, int s) {
+    int pos = find(v.begin(), v.end(), s) - v.begin();
+    return pos;
+}
 
 int main() {
     // 0. Load features
@@ -75,4 +119,9 @@ int main() {
     string trainFile = "data/test_data.csv";
     map<string, vector<string> > dat = loadCSV(trainFile);
     printCSV(dat);
+
+    // test 
+    vector<int> v = {0, 1, 2, 3, 4};
+    cout << indexInVector(v, 3) << endl;
+
 }
